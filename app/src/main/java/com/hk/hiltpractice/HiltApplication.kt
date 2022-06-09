d86dfd6c983b44051165a6ad47cb5cea119ec7c4
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2022-06-09 08:04:24 +0200

    Create HiltApplication.kt

---
+++ app/src/main/java/com/hk/hiltpractice/HiltApplication.kt
@@ -0,0 +1,13 @@
+package com.hk.hiltpractice
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by hassankhamis on 09,June,2022
+ */
+
+@HiltAndroidApp
+class HiltApplication: Application() {
+
+}
